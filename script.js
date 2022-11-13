d4a56eb43a5d5a9c64cb88ce4ab96627ea96f3a9
Matthias Bandemer <git@bandemer.net>
2022-11-13 10:56:25 +0100

    Erste Version mit Mensa Auswahl

--- script.js
+++ script.js
@@ -33,17 +33,48 @@ function showSpeiseplan(tag)
     document.getElementById('speiseplan').innerHTML = html;
 }
 
-let tag = new Date();
-fetch('https://api.studentenwerk-dresden.de/openmensa/v2/canteens/6/days/' +
-    tag.toISOString().substr(0,10) + '/meals')
-    .then((response) => response.json())
-    .then((data) => setData(data, 'heute'));
+function fetchData(mensaid) {
+    let tag = new Date();
+    fetch('https://api.studentenwerk-dresden.de/openmensa/v2/canteens/' +
+        mensaid + '/days/' + tag.toISOString().substr(0, 10) + '/meals')
+        .then((response) => response.json())
+        .then((data) => setData(data, 'heute'));
 
-tag.setDate(tag.getDate() + 1)
-fetch('https://api.studentenwerk-dresden.de/openmensa/v2/canteens/6/days/' +
-    tag.toISOString().substr(0,10) + '/meals')
-    .then((response) => response.json())
-    .then((data) => setData(data, 'morgen'));
+    tag.setDate(tag.getDate() + 1)
+    fetch('https://api.studentenwerk-dresden.de/openmensa/v2/canteens/' +
+        mensaid + '/days/' + tag.toISOString().substr(0, 10) + '/meals')
+        .then((response) => response.json())
+        .then((data) => setData(data, 'morgen'));
+}
 
+function fetchMensen() {
+    fetch('https://api.studentenwerk-dresden.de/openmensa/v2/canteens')
+        .then((response) => response.json())
+        .then((data) => {
+            let html = '<option value="0">Bitte wähle eine Mensa!</option>';
+            data.forEach((mensa) => {
+                html += '<option value="' + mensa.id + '">' + mensa.name + '</option>';
+            });
+            document.getElementById('mensenselect').innerHTML = html;
+        });
+}
 document.getElementById('heute').addEventListener('click', (event) => {showSpeiseplan('heute')});
 document.getElementById('morgen').addEventListener('click', (event) => {showSpeiseplan('morgen')});
+
+document.getElementById('savemensa').addEventListener('click', (event) => {
+    document.getElementById('modal_1').checked = false;
+    let sel = document.getElementById('mensenselect');
+    document.getElementById('spltitle').innerText = 'Speiseplan ' + sel.options[sel.selectedIndex].text;
+    fetchData(sel.value);
+});
+
+document.onkeydown = function(e){
+    if (e.keyCode == 27) {
+        var mods = document.querySelectorAll('.modal > [type=checkbox]');
+        [].forEach.call(mods, function(mod){ mod.checked = false; });
+    }
+}
+
+addEventListener('DOMContentLoaded', (event) => {
+    fetchMensen()
+});
